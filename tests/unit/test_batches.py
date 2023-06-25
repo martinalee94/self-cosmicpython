98dc33aebb946bf8242e4e87927aa5f2363dbf4d
martinalee94 <martinalee0528@gmail.com>
2023-06-25 18:49:10 -0400

    Update test in test_batches
    - Rename 'order' to 'line'
    - Update model field name

--- tests/unit/test_batches.py
+++ tests/unit/test_batches.py
@@ -18,24 +18,24 @@ def make_batch_and_line(sku, batch_qty, line_qty):
 
 def test_allocating_to_a_batch_reduces_the_available_quantity():
     # GIVEN
-    order = OrderLine(id="Order-001", sku="SMALL-TABLE", quantity=2)
+    line = OrderLine(id="Order-001", sku="SMALL-TABLE", quantity=2)
     batch = Batch(ref="batch-001", sku="SMALL-TABLE", quantity=20, eta=today)
 
     # WHEN
-    batch.allocate(order_line=order)
+    batch.allocate(order_line=line)
 
     # THEN
-    assert batch.quantity == 18
+    assert batch.available_quantity == 18
 
 
 def test_can_allocate_if_available_greater_than_required():
     # GIVEN
-    large_batch, small_order = make_batch_and_line(
+    large_batch, small_line = make_batch_and_line(
         sku="SMALL-TABLE", batch_qty=20, line_qty=1
     )
 
     # WHEN
-    result = large_batch.can_allocate(order_line=small_order)
+    result = large_batch.can_allocate(order_line=small_line)
 
     # THEN
     assert result is True
@@ -43,12 +43,12 @@ def test_can_allocate_if_available_greater_than_required():
 
 def test_cannot_allocate_if_available_smaller_than_required():
     # GIVEN
-    small_batch, large_order = make_batch_and_line(
+    small_batch, large_line = make_batch_and_line(
         sku="SMALL-TABLE", batch_qty=2, line_qty=10
     )
 
     # WHEN
-    result = small_batch.can_allocate(order_line=large_order)
+    result = small_batch.can_allocate(order_line=large_line)
 
     # THEN
     assert result is False
@@ -56,10 +56,10 @@ def test_cannot_allocate_if_available_smaller_than_required():
 
 def test_can_allocate_if_available_equal_to_required():
     # GIVEN
-    batch, order = make_batch_and_line(sku="SMALL-TABLE", batch_qty=20, line_qty=20)
+    batch, line = make_batch_and_line(sku="SMALL-TABLE", batch_qty=20, line_qty=20)
 
     # WHEN
-    result = batch.can_allocate(order_line=order)
+    result = batch.can_allocate(order_line=line)
 
     # THEN
     assert result is True
@@ -67,11 +67,11 @@ def test_can_allocate_if_available_equal_to_required():
 
 def test_cannot_allocate_if_skus_do_not_match():
     # GIVEN
-    order = OrderLine(id="Order-001", sku="SMALL-TABLE", quantity=2)
+    line = OrderLine(id="Order-001", sku="SMALL-TABLE", quantity=2)
     batch = Batch(ref="batch-001", sku="BIG-TABLE", quantity=20, eta=today)
 
     # WHEN
-    result = batch.can_allocate(order_line=order)
+    result = batch.can_allocate(order_line=line)
 
     # THEN
     assert result is False
