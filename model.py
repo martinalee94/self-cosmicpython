c623691a7d22c23bd93cafb5a0c5d2c7de8d77ca
martinalee94 <martinalee0528@gmail.com>
2023-06-25 17:48:28 -0400

    Update OrderLine model to frozen dataclass

--- model.py
+++ model.py
@@ -2,20 +2,21 @@ from datetime import datetime
 from dataclasses import dataclass
 from typing import Optional
 
-@dataclass
+
+@dataclass(frozen=True)  # immutable dataclass with no behavior
 class OrderLine:
-    id:str
-    sku:str
-    quantity:int
-    
+    id: str
+    sku: str
+    quantity: int
+
+
 @dataclass
 class Batch:
-    def __init__(self, ref:str, sku:str, q: int, eta:Optional[datetime]):
+    def __init__(self, ref: str, sku: str, q: int, eta: Optional[datetime]):
         self.reference = ref
         self.sku = sku
         self.quantity = q
         self.eta = eta
-        
-    def allocate(self,order_line: OrderLine):
+
+    def allocate(self, order_line: OrderLine):
         order_line.quantity -= self.quantity
-    
